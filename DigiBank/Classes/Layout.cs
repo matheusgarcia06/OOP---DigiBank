0f214f9a25d70c5904e69da36a5d3be83ef97f3c
Matheus Nogueira <matheus.garcianogueira@hotmail.com>
2022-02-17 18:37:20 -0300

    Criando a tela Layout

---
+++ DigiBank/Classes/Layout.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiBank.Classes
+{
+    public class Layout
+    {
+        private static int opcao = 0;
+        public static void TelaPrincipal()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Clear();
+
+            Console.WriteLine("                                                  ");
+            Console.WriteLine("                Digite a Opção desejada:          ");
+            Console.WriteLine("               =============================      ");
+            Console.WriteLine("                1 - Criar Conta:                  ");
+            Console.WriteLine("               =============================      ");
+            Console.WriteLine("                2 - Entrar com CPF e Senha:       ");
+            Console.WriteLine("               =============================      ");
+
+            opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    Console.WriteLine("Opção 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Opção 2");
+                    break;
+                default:
+                    Console.WriteLine("Opção Inválida!");
+                    break;
+            }
+
+        }
+    }
+}
--- DigiBank/Program.cs
+++ DigiBank/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DigiBank.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace DigiBank
     {
         static void Main(string[] args)
         {
+            Layout.TelaPrincipal();
+
+            Console.ReadLine();
         }
     }
 }
